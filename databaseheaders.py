f413ed5293ed387bc04a2db0a75d26b43e4f8fb7
Georgina Inkpen <116553390+GeorgeInkpen@users.noreply.github.com>
2023-03-06 12:11:36 +0000

    Add files via upload

---
+++ databaseheaders.py
@@ -0,0 +1,15 @@
+from getpass import getpass
+import mysql.connector
+
+mydb = mysql.connector.connect( #just for less typing
+  host="localhost",       #connects you to the mysqlinstance i think
+  user=input("Please enter user name: "),
+  password=getpass("Please enter password: ")
+)
+
+
+mycursor = mydb.cursor()   #just for less typing
+mycursor.execute("CREATE DATABASE IF NOT EXISTS leaderboard")
+mycursor.execute("USE leaderboard")  #which database to use
+mycursor.execute("CREATE TABLE IF NOT EXISTS players (name1 VARCHAR(255), wins INT DEFAULT 0, draws INT DEFAULT 0, loses INT DEFAULT 0)")
+
